c2945c7b3075a0bab8b31f707e4346f6f2f85f57
zhang-anzhi <zhang_anzhi@126.com>
2020-08-23 21:50:25 +0800

    remove CoolQ

--- make_release.bat
+++ make_release.bat
@@ -21,16 +21,12 @@ copy readme.md release\%name%-%ver%\readme.md
 copy requirements.txt release\%name%-%ver%\requirements.txt
 copy LICENSE release\%name%-%ver%\LICENSE
 
-REM CoolQ and CoolQAPI-MCDR
+REM Main file
 copy CoolQAPI-MCDR.py release\%name%-%ver%\CoolQAPI-MCDR.py
-copy CoolQ.zip release\%name%-%ver%\CoolQ.zip
-
-REM CoolQAPI folder
-mkdir release\%name%-%ver%\CoolQAPI
-xcopy /e /y /q CoolQAPI release\%name%-%ver%\CoolQAPI
+xcopy /e /y /q /i CoolQAPI release\%name%-%ver%\CoolQAPI
 xcopy /e /y /q /i QQBridge release\%name%-%ver%\QQBridge
 
-REM zip
+REM Zip
 cd release
 zip -r -q %name%-%ver%.zip %name%-%ver%
 rd /s /q %name%-%ver%
