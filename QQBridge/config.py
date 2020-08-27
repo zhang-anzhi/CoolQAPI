b9dfeeac6eb0b68b2680a88ccf5fc4e2b81c9132
zhang-anzhi <zhang_anzhi@126.com>
2020-08-27 17:17:13 +0800

    Console and more config

--- QQBridge/config.py
+++ QQBridge/config.py
@@ -4,27 +4,53 @@ import yaml
 
 
 class Config:
-    def __init__(self, file_path):
-        self.data = None
-        self.file_path = file_path
+    def __init__(self, logger):
+        self.data = {}
+        self.logger = logger
+        self.file_path = 'config.yml'
+        self.reload()
+
+    def reload(self):
+        self.data = {}
         if os.path.isfile(self.file_path):
-            with open(self.file_path, encoding='utf8') as file:
-                self.data = yaml.safe_load(file)
+            self._load()
         self.check_config()
 
     def touch(self, key, default):
-        if self.data is None:
-            self.data = {}
         if key not in self.data:
+            self.logger.warning(f'Use default config {default} '
+                                f'for missing option {key}')
             self.data[key] = default
-            with open(self.file_path, 'a', encoding='utf8') as file:
-                yaml.safe_dump({key: default}, file)
+            self._add(key, default)
 
     def check_config(self):
         self.touch('post_host', '127.0.0.1')
         self.touch('post_port', 5701)
         self.touch('post_url', '/post')
-        self.touch('server_list', [])
+        self.touch('server_list', {
+            'example': {
+                'host': '127.0.0.1',
+                'port': 5702,
+                'url': 'post'
+            }
+        })
+        self.touch('debug_mode', False)
+
+    def _load(self):
+        with open(self.file_path, encoding='utf8') as f:
+            self.data = yaml.safe_load(f)
+
+    def _save(self):
+        with open(self.file_path, 'w', encoding='utf8') as f:
+            yaml.dump(self.data, f)
+
+    def _add(self, key, value):
+        with open(self.file_path, 'a', encoding='utf8') as f:
+            yaml.dump({key: value}, f)
 
     def __getitem__(self, item):
-        return self.data[item]
+        try:
+            return self.data[item]
+        except KeyError:
+            self.logger.error('Configuration item missing, checking')
+            self.check_config()
--- QQBridge/logger.py
+++ QQBridge/logger.py
@@ -2,12 +2,31 @@
 import logging
 import time
 import os
-import zipfile
+from colorlog import ColoredFormatter
 
 
 class Logger:
-    fmt = logging.Formatter('[%(asctime)s] [%(levelname)s]: %(message)s',
-                            datefmt='%Y-%m-%d %H:%M:%S')
+    console_fmt = ColoredFormatter(
+        '[%(asctime)s] [%(log_color)s%(levelname)s%(reset)s]: '
+        '%(message_log_color)s%(message)s%(reset)s',
+        log_colors={
+            'DEBUG': 'blue',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'bold_red',
+        },
+        secondary_log_colors={
+            'message': {
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'bold_red'
+            }
+        },
+        datefmt='%H:%M:%S'
+    )
+    file_fmt = logging.Formatter('[%(asctime)s] [%(levelname)s]: %(message)s',
+                                 datefmt='%Y-%m-%d %H:%M:%S', )
 
     def __init__(self):
         logging.getLogger('werkzeug').setLevel(logging.ERROR)
@@ -15,15 +34,21 @@ class Logger:
         if not os.path.isdir('logs'):
             os.mkdir('logs')
         self.set_file()
+
+        # Logger
         self.logger = logging.getLogger('QQBridge')
         self.logger.setLevel(logging.INFO)
+
+        # Console Handler
         self.ch = logging.StreamHandler()
         self.ch.setLevel(logging.INFO)
-        self.ch.setFormatter(self.fmt)
+        self.ch.setFormatter(self.console_fmt)
         self.logger.addHandler(self.ch)
+
+        # File handler
         self.fh = logging.FileHandler(self.file_path, encoding='utf-8')
         self.fh.setLevel(logging.INFO)
-        self.fh.setFormatter(self.fmt)
+        self.fh.setFormatter(self.file_fmt)
         self.logger.addHandler(self.fh)
 
         self.debug = self.logger.debug
@@ -34,21 +59,17 @@ class Logger:
         self.exception = self.logger.exception
 
     def set_file(self):
-        try:
-            self.logger.removeHandler(self.fh)
-        except AttributeError:
-            pass
         if os.path.isfile(self.file_path):
             modify_time = time.strftime('%Y-%m-%d', time.localtime(
                 os.stat(self.file_path).st_mtime))
-            counter = 0
-            while True:
-                counter += 1
-                zip_file_name = 'logs/{}-{}.zip'.format(modify_time, counter)
-                if not os.path.isfile(zip_file_name):
+            c = 0
+            while c := c + 1:
+                backup_file_name = f'logs/{modify_time}-{c}.log'
+                if not os.path.isfile(backup_file_name):
+                    os.rename('logs/log.log', backup_file_name)
                     break
-            zipf = zipfile.ZipFile(zip_file_name, 'w')
-            zipf.write(self.file_path, arcname=os.path.basename(self.file_path),
-                       compress_type=zipfile.ZIP_DEFLATED)
-            zipf.close()
-            os.remove(self.file_path)
+
+    def set_level(self, level):
+        self.logger.setLevel(level)
+        self.ch.setLevel(level)
+        self.fh.setLevel(level)
